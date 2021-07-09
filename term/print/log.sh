7a2ee632dedebabd036de7a3cf478f06902351eb
AXON <axonasif@gmail.com>
2021-07-09 11:33:24 +0600

    feat: Add string

--- term/print/log.sh
+++ term/print/log.sh
@@ -12,6 +12,13 @@ println::error() {
 	local _command="$1";
 	test "${_PRINT_HELP:-no}" == yes && print_help >&2
 
-	echo -e "[!!!] ${BRED}ERROR${RC}[$_return_code]: $_source[$BASH_LINENO]: $_command";
-	exit $_return_code;
+	echo -e "[!!!] \033[1;31mERROR\033[0m[$_return_code]: $_source[$BASH_LINENO]: $_command";
+	case "${JOB:-"exit"}" in
+		exit)
+			exit $_return_code;
+		;;
+		kill)
+			kill -9 "$BASHPID"
+		;;
+	esac
 }
