52786bb2ca51e75dee6edcf397ee4ec0e039c329
AXON <axonasif@gmail.com>
2021-07-15 07:52:29 +0600

    chore: Add Bashbox.meta

--- argbash/common.sh
+++
@@ -1,6 +0,0 @@
-
-begins_with_short_option() {
-	local first_option all_short_options='h'
-	first_option="${1:0:1}"
-	test "$all_short_options" = "${all_short_options/$first_option/}" && return 1 || return 0
-}
