7f097201be5c51aaf5be959dde4f53ebfa5caead
AXON <axonasif@gmail.com>
2021-07-29 18:26:07 +0600

    refactor(print): log

---
+++ src/print/log.sh
@@ -0,0 +1,53 @@
+# Initial implementation that never made out even after being more dynamic due to performance thoughts.
+# function log() {
+# 	local _retcode="$?";
+# 	local _args_count="$#";
+	
+# 	if test "$_args_count" -gt 1; then {
+# 		local _type="$1";
+# 	} fi
+	
+# 	if test -v "_type"; then {
+# 		case "$_type" in
+# 			"INFO")
+# 				: "[%%%] \033[1;37minfo\033[0m";
+# 			;;
+# 			"WARN") 
+# 				: "[***] \033[1;37mwarn\033[0m";
+# 			;;
+# 			"ERROR")
+# 				: "[!!!] \033[1;31merror\033[0m";
+# 			;;
+# 		esac
+# 		local _msg="$_"
+# 		if test "${_type:-}" != "ERROR"; then {
+# 			echo -e "${_msg}: $2"
+# 		} else {
+# 			if test "$_args_count" -eq 3 && [[ "${!_args_count}" =~ [0-9] ]]; then {
+# 				_retcode="${!_args_count}";
+# 			} fi
+# 			local _source="${BB_ERR_SOURCE:-"${BASH_SOURCE[-1]}"}";
+# 			>&2 echo -e "${_msg}[$_retcode]: ${_source}[$BASH_LINENO]: $2";
+# 			return "$_retcode";
+# 		} fi
+# 	} else {
+# 		echo -e "$1";
+# 	} fi
+	
+# }
+
+function log::info() {
+	echo -e "[%%%] \033[1;37minfo\033[0m: $@";
+}
+
+function log::warn() {
+	echo -e "[***] \033[1;37mwarn\033[0m: $@";
+}
+
+function log::error() {
+	local _retcode="${2:-$?}";
+	local _source="${BB_ERR_SOURCE:-"${BASH_SOURCE[-1]}"}";
+
+	>&2 echo -e "[!!!] \033[1;31merror\033[0m[$_retcode]: ${_source}[$BASH_LINENO]: $1";
+	return "$_retcode";
+}
