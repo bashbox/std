04f2733a14e2de8eea809fb3a16ccff7bb61004e
AXON <axonasif@gmail.com>
2021-08-16 10:58:38 +0600

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # std
+
+A good amount of code was taken from https://github.com/dylanaraps/pure-bash-bible
--- src/native/sleep.sh
+++ src/native/sleep.sh
@@ -1,5 +1,3 @@
 bsleep() {
-    # Usage: read_sleep 1
-    #        read_sleep 0.2
-    read -rt "$1" <> <(:) || :
+    read -rt "$1" <> <(:) || :;
 }
