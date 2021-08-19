66cfc2b6e78e14c04e4a3cc4d19e7dea76dea689
AXON <axonasif@gmail.com>
2021-08-19 19:22:46 +0600

    feat: Add io::stdio

---
+++ src/io/stdio.sh
@@ -0,0 +1,14 @@
+function io::stdio::to_file() {
+	local _stdout_target="$1";
+	local _stderr_target="$2";
+	local _file;
+	# Ensure to wipe out previously created files
+	for _file in "$_stdout_target" "$_stderr_target"; do {
+		if test -e "$_file"; then {
+			rm "$_file";
+		} fi
+	} done
+	# Main action
+	STD_IO_PPID=$$ exec > >(while test -e "/proc/$STD_IO_PPID" && read -r ___stdout; do echo "$___stdout" && echo "$___stdout" >> stdout.log; done) \
+		2> >(test -e "/proc/$STD_IO_PPID" && while read -r ___stderr >&2; do echo "$___stderr" && echo "$___stderr" >> stderr.lol; done);
+}
