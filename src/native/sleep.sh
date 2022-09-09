65605eba9e8a043b79e0f0073b1d1868332c80a1
AXON <axonasif@gmail.com>
2022-09-09 02:45:01 +0000

    fix(native): Support MacOS for sleep

--- src/native/sleep.sh
+++ src/native/sleep.sh
@@ -1,4 +1,14 @@
-### From https://github.com/dylanaraps/pure-bash-bible
+### From https://stackoverflow.com/a/47521066/11010941
 sleep() {
-    read -rt "$1" <> <(:) || :;
+    local IFS
+    [[ -n "${_snore_fd:-}" ]] || { exec {_snore_fd}<> <(:); } 2>/dev/null ||
+    {
+        # workaround for MacOS and similar systems
+        local fifo
+        fifo=$(mktemp -u)
+        mkfifo -m 700 "$fifo"
+        exec {_snore_fd}<>"$fifo"
+        rm "$fifo"
+    }
+    read ${1:+-t "$1"} -u $_snore_fd || :
 }
