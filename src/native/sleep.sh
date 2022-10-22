d35dafda2f15efdf9c78c95b6e7e6673b021e7fa
AXON <axonasif@gmail.com>
2022-10-22 17:07:06 +0000

    refactor: native/sleep

--- src/native/sleep.sh
+++ src/native/sleep.sh
@@ -1,14 +1,12 @@
 ### From https://stackoverflow.com/a/47521066/11010941
 sleep() {
-    local IFS
-    [[ -n "${_snore_fd:-}" ]] || { exec {_snore_fd}<> <(:); } 2>/dev/null ||
-    {
+    [[ -n "${_snore_fd:-}" ]] || { exec {_snore_fd}<> <(:); } 2>/dev/null || {
         # workaround for MacOS and similar systems
-        local fifo
-        fifo=$(mktemp -u)
-        mkfifo -m 700 "$fifo"
-        exec {_snore_fd}<>"$fifo"
-        rm "$fifo"
+        local fifo;
+        fifo=$(mktemp -u);
+        mkfifo -m 700 "$fifo";
+        exec {_snore_fd}<>"$fifo";
+        rm "$fifo";
     }
-    read ${1:+-t "$1"} -u $_snore_fd || :
+    IFS='' read ${1:+-t "$1"} -u $_snore_fd || :
 }
