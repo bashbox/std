6ac7a3c9f28da41df3ba5251b8ae7074eca99b21
AXON <axonasif@gmail.com>
2022-10-22 17:35:44 +0000

    feat: process/lockfile

---
+++ src/process/lockfile.sh
@@ -0,0 +1,16 @@
+use std::process::get_temp;
+
+function lockfile() {
+	local name="$1";
+	local lock_file;
+	lock_file="$(get_temp::dir)/.${name}.lock";
+	if test -e "$lock_file"; then {
+		log::info "Waiting for another job to finish";
+		until test ! -e "$lock_file" || ! { kill -0 "$(< "$lock_file")"; } 2>/dev/null; do {
+			sleep 0.5;
+		} done
+	} fi
+	printf '%s\n' "$$" > "$lock_file";
+	# shellcheck disable=SC2064
+	trap "rm -f '$lock_file' 2>/dev/null" EXIT;
+}
