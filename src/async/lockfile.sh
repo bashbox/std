ecc112145109e1870de6a6cbf5233da59b736657
AXON <axonasif@gmail.com>
2022-10-28 13:08:59 +0000

    fix: deadlock on rare leftover lockfile

--- src/async/lockfile.sh
+++ src/async/lockfile.sh
@@ -2,23 +2,28 @@ use box::process::get_temp;
 use box::builtin::trap::append;
 
 function lockfile() {
-	
+
 	local name="$1";
 	local lock_file;
 	lock_file="$(get_temp::dir)/.${name}.lock";
 
 	# Print info when possible
 	if test -e "$lock_file"; then {
+		if ! { kill -0 "$(< "$lock_file")"; } 2>/dev/null; then {
+			rm "$lock_file" 2>/dev/null || :;
+		} fi
 		log::info "Awaiting for another ${name} job to finish";
 	} fi
 
 	# Await for an existing process
-	while { kill -0 "$(< "$lock_file")"; } 2>/dev/null; do {
-		sleep 0.5;
+	for _ in {1..5}; do {
+		while { kill -0 "$(< "$lock_file")"; } 2>/dev/null; do {
+			sleep 0.5;
+		} done
 	} done
 
 	# Await for locking
-	until (set -o noclobber; printf '%s\n' "$$" > "$lock_file") 2>/dev/null; do {
+	until (set -o noclobber && printf '%s\n' "$$" > "$lock_file") 2>/dev/null; do {
 		sleep 0.5;
 	} done
 	
