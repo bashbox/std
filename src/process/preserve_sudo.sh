07f4cb54a58750b3bcc5b408cb6a3f1a2efb6054
AXON <axonasif@gmail.com>
2022-11-04 20:44:11 +0600

    Create preserve_sudo.sh

---
+++ src/process/preserve_sudo.sh
@@ -0,0 +1,18 @@
+use box::print::log;
+use box::native::sleep;
+
+function process::preserve_sudo {
+  if test "$EUID" -ne 0; then {
+    if ! sudo -nv 2>/dev/null; then {
+      log::warn "Build command needs root for some operations, reqesting root...";
+      sudo -v;
+
+      # Perserve the root access
+      (
+        while sleep 30 && { kill -0 "$___self_PID"; } 2>/dev/null; do {
+          sudo -v;
+        } done
+      ) &
+    } fi
+  } fi
+}
